92236e047f32d0237d10200dd2d9e9964ff6f4f9
wallecnik <tomas.valka@gmail.com>
2015-03-03 21:40:36 +0100

    Skeleton of MissionManagerImpl tests

---
+++ src/cz/muni/fi/agentproject/tests/MissionManagerImplTest.java
@@ -0,0 +1,43 @@
+package cz.muni.fi.agentproject.tests;
+
+import cz.muni.fi.agentproject.MissionManager;
+import cz.muni.fi.agentproject.MissionManagerImpl;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author  Wallecnik
+ * @version 31.22.2015
+ */
+public class MissionManagerImplTest {
+
+    MissionManager manager;
+
+    @Before
+    public void setUp() {
+        manager = new MissionManagerImpl();
+    }
+
+    @Test
+    public void createMission() {
+
+    }
+
+    @Test
+    public void getMission() {
+
+    }
+
+    @Test
+    public void updateMission() {
+
+    }
+
+    @Test
+    public void deleteMission() {
+
+    }
+
+}
+
