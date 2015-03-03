56de48d16c0551124cc5bc4dcd51b349bd514ddc
wallecnik <tomas.valka@gmail.com>
2015-03-03 17:01:28 +0100

    Rough Agent class created

--- src/Agent.java
+++ src/Agent.java
@@ -2,4 +2,16 @@
  * Created by Wallecnik on 03.03.15.
  */
 public class Agent {
+
+    private Long id;
+    private String name;
+    private Long born;
+    private String description;
+
+    public Agent(Long id, String name, Long born, String description) {
+        this.id = id;
+        this.name = name;
+        this.born = born;
+        this.description = description;
+    }
 }
